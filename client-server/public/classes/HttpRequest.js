03c846389791b72e3f9da25b496c44fd1db3b413
Wellinton Hideki <wellintonhideki@gmail.com>
2021-04-08 15:48:59 -0300

    client-server

---
+++ client-server/public/classes/HttpRequest.js
@@ -0,0 +1,59 @@
+class HttpRequest {
+
+    static get(url, params = {}){
+
+        return HttpRequest.request('GET', url, params);
+    }
+
+    static delete(url, params = {}){
+
+        return HttpRequest.request('DELETE', url, params);
+    }
+
+    static put(url, params = {}){
+
+        return HttpRequest.request('PUT', url, params);
+    }
+
+    static post(url, params = {}){
+
+        return HttpRequest.request('POST', url, params);
+    }
+
+    // static permite chamar direto
+    static request(method, url, params = {}){
+
+        return new Promise((resolve, reject) => {
+
+            let ajax = new XMLHttpRequest();      
+
+            ajax.open(method.toUpperCase(),url);
+
+            ajax.onerror = e => {
+
+                reject(e);
+            }
+    
+            ajax.onload = event => {
+    
+                let obj = {};
+    
+                try {
+    
+                    obj = JSON.parse(ajax.responseText);
+    
+                } catch (e) {
+    
+                    reject(e);
+                    console.error(e);
+                }
+                
+                resolve(obj);
+    
+            };
+    
+            ajax.setRequestHeader('Content-type', 'application/json')
+            ajax.send(JSON.stringify(params));
+        });
+    }
+}
